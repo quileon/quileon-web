c189f93809a659d4f77b4dfaf598049c76225739
Shaquille Arriza Hidayat <121283381+Quillzyy@users.noreply.github.com>
2025-01-19 17:26:32 +0000

    Add startup script and more codespace configuration

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# requirements
+sudo apt update && sudo apt install -y gpg
+
+# eza installation
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update && sudo apt install -y eza
+
+# zoxide
+sudo apt update && sudo apt install -y zoxide
