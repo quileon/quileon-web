f9a56c8b46a0a381c32ae112134f38c49b6d88a0
Shaquille Arriza Hidayat <121283381+Quillzyy@users.noreply.github.com>
2025-01-23 17:40:10 +0000

    Modify `header.tsx` into a server component
    Separate `header.tsx` hooks stuff into `header-dropdown.tsx`

--- src/components/header/header-dropdown.tsx
+++ src/components/header/header-dropdown.tsx
@@ -1,29 +1,50 @@
+"use client";
+
 import { Smooch_Sans } from "next/font/google";
 import Link from "next/link";
+import { useState } from "react";
+import CaretDown from "@public/CaretDown";
+import CaretUp from "@public/CaretUp";
 
 const smoochSans = Smooch_Sans({
   subsets: ["latin"],
 });
 
 export default function HeaderDropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleDropdown() {
+    setIsOpen(!isOpen);
+  }
   return (
-    <nav
-      className={`${smoochSans.className} font-bold text-4xl text-pastel-brown`}
-    >
-      <ul className="flex flex-col gap-2 absolute bg-pastel-grey w-full">
-        <li className="hover:bg-pastel-pink px-4">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="hover:bg-pastel-pink px-4">
-          <Link href="/about">About</Link>
-        </li>
-        <li className="hover:bg-pastel-pink px-4">
-          <Link href="/work">Work</Link>
-        </li>
-        <li className="hover:bg-pastel-pink px-4">
-          <Link href="/blogs">Blogs</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <div onClick={toggleDropdown} className="cursor-pointer lg:hidden">
+        {isOpen ? (
+          <CaretUp height={15} width={25} />
+        ) : (
+          <CaretDown height={15} width={25} />
+        )}
+      </div>
+      {isOpen && (
+        <nav
+          className={`${smoochSans.className} font-bold text-4xl text-pastel-brown absolute w-full top-16 left-0`}
+        >
+          <ul className="flex flex-col gap-2 absolute bg-pastel-grey w-full">
+            <li className="hover:bg-pastel-pink px-4">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:bg-pastel-pink px-4">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="hover:bg-pastel-pink px-4">
+              <Link href="/work">Work</Link>
+            </li>
+            <li className="hover:bg-pastel-pink px-4">
+              <Link href="/blogs">Blogs</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
   );
 }
