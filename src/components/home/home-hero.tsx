6795df2b06d8b4cbc1ba182482066aeda6a6be23
Shaquille Arriza Hidayat <121283381+Quillzyy@users.noreply.github.com>
2025-01-24 04:54:09 +0000

    Create a hero element on homepage

---
+++ src/components/home/home-hero.tsx
@@ -0,0 +1,13 @@
+export default function HomeHero() {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="font-bold text-4xl">
+        Network Engineer and Cloud Engineer
+      </h1>
+      <p className="text-xl">
+        I am Shaquille, an undergraduate network engineer from Politeknik Negeri
+        Jakarta. My passion is Linux and VIM.
+      </p>
+    </div>
+  );
+}
