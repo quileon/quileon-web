c92724ed746b05ac15403a826082048be3b3d408
Shaquille Arriza Hidayat <121283381+Quillzyy@users.noreply.github.com>
2025-01-24 08:56:00 +0000

    Finishing hero element
    Adding links to about page on hero element.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import HomeHero from "@/components/home/home-hero";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="container max-w-screen-lg mx-auto px-4 py-8">
+    <div
+      className={`container max-w-screen-lg mx-auto px-4 py-8 ${inter.className}`}
+    >
       <HomeHero />
     </div>
   );
