9dfe8c0a04d67e9fe9dfcd5db218be0bb54cf703
Shaquille Arriza Hidayat <121283381+Quillzyy@users.noreply.github.com>
2025-01-30 09:33:32 +0000

    Add analytics as well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <main className="flex-grow">{children}</main>
         <Footer />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
