c57c5e295db0ed2c6760dd38f7608b01bcc197a2
Shaquille Arriza Hidayat <121283381+Quillzyy@users.noreply.github.com>
2025-01-30 03:50:58 +0000

    Add footer to page

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,32 @@
+import { Smooch_Sans } from "next/font/google";
+import LinkedIn from "@public/linkedin";
+import GitHub from "@public/github";
+import Link from "next/link";
+
+const smoochSans = Smooch_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export default function Footer() {
+  return (
+    <footer
+      className={`bg-pastel-brown ${smoochSans.className} flex flex-col items-center justify-center gap-1 h-20 pt-2`}
+    >
+      <nav className="flex flex-row gap-4">
+        <Link href="https://github.com/Quillzyy" target="_blank">
+          <GitHub className="h-5 w-5" />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/shaquille-arriza-hidayat"
+          target="_blank"
+        >
+          <LinkedIn className="h-5 w-5" />
+        </Link>
+      </nav>
+      <h2 className="text-2xl font-bold text-pastel-white">
+        quillzyy powered 2024
+      </h2>
+    </footer>
+  );
+}
